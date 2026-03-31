6cd84b79beced408c5883f5f6f21ecb8d2cb905f
ItsMeJoji <itsmejojitcg@gmail.com>
2026-03-30 22:45:48 -0700

    0.13.2 - Updating sitemap generation

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,28 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import Sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig({
     plugins: [
         tailwindcss(),
+        Sitemap({
+            hostname: 'https://poketrainer.tools',
+            dynamicRoutes: [
+                '/pokemon-lookup',
+                '/encounter',
+                '/catch-rate',
+                '/shiny-odds',
+                '/sos-tracker',
+                '/mmo-permutations',
+                '/ribbon-tracker',
+                '/info/shiny-hunting',
+                '/info/sos-hunting',
+                '/info/mmo-guide',
+                '/info/ribbon-master-guide',
+                '/contact',
+                '/privacy',
+            ],
+        }),
     ],
     base: '/'
 })
